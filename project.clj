3dc1fd9d5d362d2ac88f50b9900095d871c3343b
Kenneth Leung <ccllnekk@gmail.com>
2017-01-05 01:56:17 -0800

    kl

--- project.clj
+++ project.clj
@@ -12,6 +12,8 @@
                  [org.apache.ant/ant-junit "1.9.7"]
                  [org.apache.ant/ant "1.9.7"]]
 
+  :plugins [[lein-codox "0.10.2"]]
+
   :profiles {:provided {:dependencies
                         [[net.mikera/cljunit "0.6.0" :scope "test"]
                          [junit/junit "4.12" :scope "test"]
@@ -20,10 +22,16 @@
              :uberjar {:aot :all}}
 
   :global-vars {*warn-on-reflection* true}
+  :target-path "out/%s"
+  :aot :all
 
-  :java-source-paths ["src/main/java"]
+  :java-source-paths ["src/main/java" "test/main/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+  :resource-paths ["src/main/resources"]
 
-  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"])
+  :jvm-opts ["-Dlog4j.configurationFile=file:attic/log4j2.xml"]
+  :javac-options ["-source" "8"
+                  "-Xlint:unchecked" "-Xlint:-options" "-Xlint:deprecation"])
+
 
