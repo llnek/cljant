30826fb53a61a43aaade07a8ffe4c1c80c861ff5
Kenneth Leung <ccllnekk@gmail.com>
2022-04-19 15:20:00 -0700

    updated to latest

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ provides a simple way to access those tasks in clojure by treating each as a fun
 
 Add the following dependency to your `project.clj` file:
 
-    [io.czlab/cljant "2.0.1"]
+    [io.czlab/cljant "2.1.0"]
 
 ## Documentation
 
@@ -57,9 +57,9 @@ Please use the project's [GitHub issues page] for all questions, ideas, etc. **P
 
 ## License
 
-Copyright © 2013-2020 Kenneth Leung
+Copyright © 2013-2022 Kenneth Leung
 
-Distributed under the Eclipse Public License either version 1.0 or (at
+Distributed under the Apache License either version 2.0 or (at
 your option) any later version.
 
 <!--- links -->
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-(defproject io.czlab/cljant "2.0.1"
+(defproject io.czlab/cljant "2.1.0"
 
   ;;add these for maven deployment
   :Xpom-addition
@@ -14,24 +14,25 @@
     "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}]]
   ;;for maven deployment
 
-  :license {:url "http://www.eclipse.org/legal/epl-v10.html"
-            :name "Eclipse Public License"}
-  :url "https://github.com/llnek/cljant"
+  :license {:url "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            :name "Apache License"}
 
   :description "Apache ant wrapped in clojure."
+  :url "https://github.com/llnek/cljant"
 
   :dependencies [;;;;[ant-contrib/ant-contrib "1.0b3" :exclusions [ant]]
                  ;;;;[org.apache.ant/ant-launcher "1.10.1"]
-                 [org.apache.ant/ant-junit4 "1.10.7" :scope "test"]
-                 [org.apache.ant/ant-junit "1.10.7" :scope "test"]
-                 [junit/junit "4.12" :scope "test"]
-                 [org.apache.ant/ant "1.10.7"]]
+                 [org.apache.ant/ant-junit4 "1.10.12" :scope "test"]
+                 [org.apache.ant/ant-junit "1.10.12" :scope "test"]
+                 [junit/junit "4.13.2" :scope "test"]
+                 [org.apache.ant/ant "1.10.12"]]
 
-  :plugins [[cider/cider-nrepl "0.22.4"]
-            [lein-codox "0.10.7"]]
+  :plugins [[cider/cider-nrepl "0.28.3"]
+            [lein-codox "0.10.8"]
+            [lein-cljsbuild "1.1.8"]]
 
   :profiles {:provided {:dependencies
-                        [[org.clojure/clojure "1.10.1" :scope "provided"]
+                        [[org.clojure/clojure "1.11.1" :scope "provided"]
                          [net.mikera/cljunit "0.7.0" :scope "test"]]}
              :uberjar {:aot :all}}
 
@@ -46,8 +47,8 @@
   :test-paths ["src/test/clojure"]
 
   :jvm-opts ["-Dlog4j.configurationFile=file:attic/log4j2.xml"]
-  :javac-options ["-source" "11"
-                  "-target" "11"
+  :javac-options ["-source" "16"
+                  "-target" "16"
                   "-Xlint:unchecked" "-Xlint:-options" "-Xlint:deprecation"])
 
 
